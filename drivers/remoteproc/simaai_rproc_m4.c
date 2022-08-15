009a5cd3016b89c8f6389aa83fe60f03b9d75678
Sriram Raghunathan <sriram.r@sima.ai>
2022-08-15 10:13:34 +0530

    Remoteproc: Introducing rempoteproc driver for M4 coprocessor
    This code is tested on Zebu
    
    Signed-off-by: Sriram Raghunathan <sriram.r@sima.ai>
    Signed-off-by: Nilesh Raghuvanshi <nilesh.r@sima.ai>

---
+++ drivers/remoteproc/simaai_rproc_m4.c
@@ -0,0 +1,356 @@
+/*
+ * SIMAAI Remote Processor driver for M4 processor family
+ *
+ */
+
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/of_address.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/remoteproc.h>
+#include <linux/of_reserved_mem.h>
+#include <linux/dma-mapping.h>
+#include "remoteproc_internal.h"
+
+// Use this for bus_addr kludge
+#define M4_SRAM_ADDR 	0x1d00000
+#define PRC_BASE 		0x30100000
+#define PRC_REG__CKG_RST_REG__MLA_CK_RST_ADDR 0x00000150
+
+#define NOC_REG_READ(x)                         (readl(x))
+#define NOC_REG_WRITE(reg, val)         (writel(val, reg))
+
+// work with only the M4-SRAM
+#define SIMA_MAX_MEM_REGIONS 1
+
+const char *m4_mem_names[SIMA_MAX_MEM_REGIONS] = {
+		"m4_sram"
+};
+
+/**
+ * struct sima_mem - sima internal memory structure
+ * @cpu_addr: MPU virtual address of the memory region
+ * @bus_addr: Bus address used to access the memory region
+ * @dev_addr: Device address from SIMA remote cores view
+ * @size: Size of the memory region
+ */
+struct sima_mem {
+	char name[20];
+	void __iomem * cpu_addr;
+	phys_addr_t bus_addr;
+	u32 dev_addr;
+	size_t size;
+};
+
+/**
+ * struct sima_rproc - sima remote processor state
+ * @mem: sima memory information
+ */
+struct sima_rproc {
+	struct sima_mem mem[SIMA_MAX_MEM_REGIONS];
+	int is_rtc_only;
+};
+
+// TODO: Fix this, stub until mailbox & virtio are brought into dts
+static void sima_rproc_kick(struct rproc *rproc, int vqid)
+{
+	return;
+}
+
+struct __attribute__((__packed__)) ResetControl {
+	uint32_t A65_POR;
+	uint32_t A65_WarmReset;
+	uint32_t A65_OtherReset;
+	uint32_t M4_Reset;
+	uint32_t padding[12];
+	uint64_t A65_C0T0_ResetVector;
+	uint64_t A65_C0T1_ResetVector;
+	uint64_t A65_C1T0_ResetVector;
+	uint64_t A65_C1T1_ResetVector;
+	uint64_t A65_C2T0_ResetVector;
+	uint64_t A65_C2T1_ResetVector;
+	uint64_t A65_C3T0_ResetVector;
+	uint64_t A65_C3T1_ResetVector;
+};
+
+typedef struct __attribute__ ((packed)) {
+	uint32_t mla_clk_en : 1;
+	uint32_t mla_ahb_clk_en : 1;
+	uint32_t mla_apb_clk_en : 1;
+	uint32_t mla_axi_clk_en : 1;
+	uint32_t mla_dbg_clk_en : 1;
+	uint32_t mla_ahb_resetn : 1;
+	uint32_t mla_apb_resetn : 1;
+	uint32_t mla_axi_resetn : 1;
+	uint32_t mla_por_rst : 1;
+	uint32_t mla_uctrl_por_rst : 1;
+	uint32_t mla_uctrl_sys_rst : 1;
+	uint32_t mla_dbg_rstn : 1;
+	uint32_t reserved0 : 20;
+} mla_ck_rst_s;
+
+typedef union __attribute__ ((packed)) {
+	mla_ck_rst_s reg;
+	uint32_t u32;
+} mla_ck_rst_u;
+
+static int sima_rproc_start(struct rproc *rproc)
+{
+	struct device *dev = rproc->dev.parent;
+	uint32_t offset = PRC_REG__CKG_RST_REG__MLA_CK_RST_ADDR;
+	void __iomem *addr = ioremap(PRC_BASE, 0x1000);
+
+	mla_ck_rst_u mla_s = {0};
+
+	mla_s.u32 = NOC_REG_READ(addr + offset);
+	mla_s.reg.mla_uctrl_por_rst = 0;
+	mla_s.reg.mla_uctrl_sys_rst = 0;
+	NOC_REG_WRITE(addr + offset, mla_s.u32);
+
+	dev_info(dev, "Started M4");
+
+	iounmap(addr);
+	return 0;
+}
+
+/* TODO:[SRIRAM] Understand more of power states, deep sleep for M4 */
+static int sima_rproc_stop(struct rproc *rproc)
+{
+	return 0;
+}
+
+static int sima_rproc_elf_load_rsc_table(struct rproc *rproc,
+                                         const struct firmware *fw)
+{
+	if (rproc_elf_load_rsc_table(rproc, fw)) {
+		dev_warn(rproc->dev.parent,
+			"no resource table found for this firmware\n");
+	}
+
+	return 0;
+}
+
+static void simaai_rproc_m4_clk_en(struct rproc *rproc)
+{
+	/* m4 program not loaded in its SRAM at this point */
+	struct sima_rproc *sproc = rproc->priv;
+	struct device *dev = rproc->dev.parent;
+	void __iomem *addr = ioremap(PRC_BASE, 0x1000);
+	uint32_t offset = PRC_REG__CKG_RST_REG__MLA_CK_RST_ADDR;
+
+	mla_ck_rst_u mla_s = {0};
+
+	mla_s.u32 = NOC_REG_READ(addr + offset);
+	mla_s.reg.mla_clk_en = 1;
+	mla_s.reg.mla_ahb_clk_en = 1;
+	mla_s.reg.mla_apb_clk_en = 1;
+	mla_s.reg.mla_axi_clk_en = 1;
+	mla_s.reg.mla_dbg_clk_en = 1;
+	//mla_s.u32 = 0x6ff;
+	NOC_REG_WRITE(addr + offset, mla_s.u32);
+
+	udelay(1000);
+
+	mla_s.u32 = NOC_REG_READ(addr + offset);
+	mla_s.reg.mla_ahb_resetn = 1;
+	mla_s.reg.mla_apb_resetn = 1;
+	mla_s.reg.mla_axi_resetn = 1;
+	mla_s.reg.mla_por_rst = 0;
+	mla_s.reg.mla_uctrl_por_rst = 1;
+	mla_s.reg.mla_uctrl_sys_rst = 1;
+	mla_s.reg.mla_dbg_rstn = 0;
+	NOC_REG_WRITE(addr + offset, mla_s.u32);
+
+	dev_dbg(dev, "Enabled M4 clock");
+
+	iounmap(addr);
+	return;
+}
+
+static int sima_rproc_elf_load(struct rproc *rproc, const struct firmware *fw)
+{
+	// Enable clock before writing.
+	simaai_rproc_m4_clk_en(rproc);
+	return rproc_elf_load_segments(rproc, fw);
+}
+
+static int simaai_rproc_mem_alloc(struct rproc *rproc,
+				struct rproc_mem_entry *mem)
+{
+	struct device *dev = rproc->dev.parent;
+	void *va;
+	
+	va = ioremap_wc(mem->dma, mem->len);
+	if (!va) {
+		dev_warn(dev, "ERROR : unable to map memory region: %pa+%zx\n",
+			 &mem->dma, mem->len);
+		return -ENOMEM;
+	}
+
+	/* Update memory entry va */
+	dev_info(dev, "da:0x%llx, len:%u va:0x%llx\n", mem->dma, mem->len, va);
+	mem->va = va;
+	return 0;
+}
+
+static int simaai_rproc_mem_release(struct rproc *rproc,
+				struct rproc_mem_entry *mem)
+{
+	iounmap(mem->va);
+	return 0;
+}
+
+static int sima_parse_fw(struct rproc *rproc, const struct firmware *fw)
+{
+	struct sima_rproc *srproc = rproc->priv;
+	struct device *dev = rproc->dev.parent;
+	struct rproc_mem_entry *mem;
+	int i;
+
+	for (i = 0; i < SIMA_MAX_MEM_REGIONS; i++) {
+		mem = rproc_mem_entry_init(dev, NULL,
+					   (dma_addr_t)srproc->mem[i].bus_addr, srproc->mem[i].size,
+						srproc->mem[i].dev_addr,
+						simaai_rproc_mem_alloc,
+						simaai_rproc_mem_release,
+						srproc->mem[i].name);
+		if (!mem) {
+			dev_err(dev, "ERROR : unable to initialize memory-region %s\n",
+						srproc->mem[i].name);
+			return -ENOMEM;
+		}
+
+		dev_info(dev, "region_name:[%s],phys_addr:[0x%lx],size:[0x%x],"
+						"bus_addr:[0x%llx],cpu_addr:[0x%llx]",
+						srproc->mem[i].name, srproc->mem[i].dev_addr,
+						srproc->mem[i].size, srproc->mem[i].bus_addr,
+						srproc->mem[i].cpu_addr);
+
+    	rproc_add_carveout(rproc, mem);
+
+	}
+
+	return sima_rproc_elf_load_rsc_table(rproc, fw);
+}
+
+static const struct rproc_ops sima_rproc_ops = {
+	.start					= sima_rproc_start,
+	.stop					= sima_rproc_stop,
+	.load               	= sima_rproc_elf_load,
+	.parse_fw	        	= sima_parse_fw,
+	.find_loaded_rsc_table 	= rproc_elf_find_loaded_rsc_table,
+	.get_boot_addr			= rproc_elf_get_boot_addr,
+	// [SRIRAM] TODO: Improve the kick to handle mbox init, i.e I/O with m4
+	.kick               	= sima_rproc_kick,
+	.sanity_check			= rproc_elf_sanity_check,
+};
+
+static const struct of_device_id sima_rproc_of_match[] = {
+	{ .compatible = "simaai,m4-rproc" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, sima_rproc_of_match);
+
+static int sima_rproc_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct sima_rproc *sproc;
+	struct device_node *np = dev->of_node;
+	const char *fw_name;
+	struct rproc *rproc;
+	struct resource *res;
+	int ret, i;
+
+	ret = of_property_read_string(dev->of_node, "sima,pm-firmware", &fw_name);
+	if (ret) {
+		dev_err(dev, "ERROR : Firmware name is missing\n");
+		return -ENODEV;
+	}
+
+	rproc = rproc_alloc(dev, np->name, &sima_rproc_ops, fw_name,
+							sizeof(*sproc));
+	if (!rproc) {
+		dev_err(dev, "ERROR : allocating rproc handle\n");
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	sproc = rproc->priv;
+	rproc->has_iommu = false;
+	sproc->is_rtc_only = true;
+	platform_set_drvdata(pdev, rproc);
+
+	for (i = 0; i < SIMA_MAX_MEM_REGIONS; i++) {
+		res = platform_get_resource_byname(pdev, IORESOURCE_MEM, m4_mem_names[i]);
+		if (!res) {
+			dev_err(dev, "ERROR : getting mem region %s\n", m4_mem_names[i]);
+			goto err_put_rproc;
+		}
+
+		sproc->mem[i].bus_addr = res->start;
+		sproc->mem[i].dev_addr = 0;
+		sproc->mem[i].size = (res->end - res->start) + 1;
+		strcpy(sproc->mem[i].name, m4_mem_names[i]);
+	}
+
+	ret = rproc_add(rproc);
+	if (ret) {
+		dev_err(dev, "ERROR : Failed to add rproc handle\n");
+		goto err_put_rproc;
+	}
+
+	dev_dbg(dev, "Successfully added rproc handle\n");
+
+err_put_rproc:
+	rproc_free(rproc);
+err:
+	return ret;
+}
+
+static int sima_rproc_remove(struct platform_device *pdev)
+{
+	struct rproc *rproc = platform_get_drvdata(pdev);
+
+	rproc_del(rproc);
+	rproc_free(rproc);
+
+	return 0;
+}
+
+#ifdef CONFIG_PM
+static int sima_rpm_suspend(struct device *dev)
+{
+	return -EBUSY;
+}
+
+static int sima_rpm_resume(struct device *dev)
+{
+	return 0;
+}
+#endif
+
+static const struct dev_pm_ops sima_rproc_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(sima_rpm_suspend, sima_rpm_resume)
+};
+
+static struct platform_driver sima_rproc_driver = {
+	.probe = sima_rproc_probe,
+	.remove = sima_rproc_remove,
+	.driver = {
+		.name = "simaai-rproc-m4",
+		.of_match_table = sima_rproc_of_match,
+		.pm = &sima_rproc_pm_ops,
+	},
+};
+
+module_platform_driver(sima_rproc_driver);
+
+MODULE_AUTHOR("Sriram Raghunathan <sriram.r@sima.ai>");
+MODULE_AUTHOR("Nilesh Raghuvanshi <nilesh.r@sima.ai>");
+MODULE_DESCRIPTION("SIMA.ai remote processor control driver for M4");
+MODULE_LICENSE("Dual MIT/GPL");
